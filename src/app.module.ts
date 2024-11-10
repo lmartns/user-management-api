bbbb321111b1ad865f7da6294fca99c2ee22e579
lmartns <leonardomartins578@gmail.com>
2024-11-10 14:19:13 -0300

    ✨feat: add global HTTP exception filter for improved error handling

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from './database/database.module';
 import { UsersModule } from './users/user.module';
+import { HttpExceptionFilter } from './utils/http-exception.filter';
+import { APP_FILTER } from '@nestjs/core';
 
 @Module({
   imports: [DatabaseModule, UsersModule],
   controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide: APP_FILTER,
+      useClass: HttpExceptionFilter,
+    },
+  ],
 })
 export class AppModule {}
